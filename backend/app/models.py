943c47f90299bebe5e4679a5b4560fab6e3995a2
marianaperna07 <marianaperna@ua.pt>
2024-10-27 16:43:58 +0000

    TODOLIST-16 correct fech data and api implementation

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
-# Modelo de Task
+# User model
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    cognito_id = Column(String(255), unique=True, index=True, nullable=False)
+    given_name = Column(String(255), nullable=False)
+    family_name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+
+    tasks = relationship("Task", back_populates="owner")
+
+# Task model
 class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
-    description = Column(String(255)) 
+    description = Column(String(255))
     is_completed = Column(Integer, default=0)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("User", back_populates="tasks")
+
